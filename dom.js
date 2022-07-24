85efa815fc359710687af3a353350ea115d7c5ca
Faiyez <smfnoor@mun.ca>
2022-07-24 10:57:25 -0230

    added functions

---
+++ dom.js
@@ -0,0 +1,12 @@
+var headerTitle = document.getElementById('#hello1.hello');
+console.log(headerTitle);
+headerTitle.textContent = 'Hello';
+//headerTitle.innterText = 'Good bye';
+//var items = document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[1]);
+//items[1].textContent = 'Hello 2';
+//items[1].style.fontWeight = 'bold';
+//items[1].style.backGround = 'yellow';
+//console.dir(document);
+//var items = document.querySelector('#items');
